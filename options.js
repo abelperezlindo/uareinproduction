11001a591746f882a38df3aa4d79b4885526159b
abelpzl <abelpzl@3637004.no-reply.drupal.org>
2023-05-30 15:33:43 -0300

    add popup

--- options.js
+++ options.js
@@ -1,10 +1,20 @@
+
 var form = document.getElementById('configForm');
-console.log('Datos no guardados');
+// Get from storage.
+const saved  = chrome.storage.local.get(["list-urls"]);
+saved.then(setDefaultText);
+
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-  var textAreaValue = document.getElementById('listUrls').value;
+  var textAreaValue = document.getElementById('list-urls').value;
   // Guardar los datos en el almacenamiento local
-  chrome.storage.local.set({ 'listUrls': textAreaValue }, function() {
+  chrome.storage.local.set({ 'list-urls': textAreaValue }, function() {
     console.log('Datos guardados');
+    window.location.reload();
   });
 });
+function setDefaultText(result){
+  document.getElementById('list-urls').defaultValue = result['list-urls'];
+  document.getElementById('list-urls').setAttribute('value', result['list-urls']);
+}
+
