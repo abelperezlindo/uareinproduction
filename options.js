5a68dc3e502ea4166fffa4b2ac5b2e1a9de70f71
abelpzl <abelpzl@3637004.no-reply.drupal.org>
2024-04-19 18:43:13 -0300

    Changes in potions page

--- options.js
+++ options.js
@@ -1,21 +1,44 @@
+// Saves options to chrome.storage
+const saveOptions = () => {
+  const color = document.getElementById('color').value;
+  const likesColor = document.getElementById('like').checked;
 
-var form = document.getElementById('configForm');
-// Get from storage.
-const saved  = chrome.storage.local.get(["list-urls"]);
-saved.then(setDefaultText);
+  chrome.storage.sync.set(
+    { favoriteColor: color, likesColor: likesColor },
+    () => {
+      // Update status to let user know options were saved.
+      const status = document.getElementById('status');
+      status.textContent = 'Options saved.';
+      setTimeout(() => {
+        status.textContent = '';
+      }, 750);
+    }
+  );
+};
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  var textAreaValue = document.getElementById('list-urls').value;
-  var parts = textAreaValue.split("\n");
-  // Guardar los datos en el almacenamiento local
-  chrome.storage.local.set({ 'list-urls': parts }, function() {
-    console.log('Datos guardados');
-    window.location.reload();
-  });
-});
-function setDefaultText(result){
-  let listAsText = result['list-urls'].join("/n");
-  document.getElementById('list-urls').defaultValue = listAsText;
-  document.getElementById('list-urls').setAttribute('value', listAsText);
+const peasteCurrentDomain = async () => {
+  let dominio = document.getElementById('domain');
+  if (!dominio.value) {
+    const host = await getCurrentTabUrl();
+    dominio.setAttribute('value', window.location.host)
+  }
+};
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+const restoreOptions = () => {
+  chrome.storage.sync.get(
+    { favoriteColor: 'red', likesColor: true },
+    (items) => {
+      document.getElementById('color').value = items.favoriteColor;
+      document.getElementById('like').checked = items.likesColor;
+    }
+  );
+};
+const getCurrentTabUrl = async () => {
+  const tabs = await chrome.tabs.query({ active: true })
+  return tabs[0].url
 }
+//document.addEventListener('DOMContentLoaded', restoreOptions);
+document.addEventListener('DOMContentLoaded', peasteCurrentDomain);
+document.getElementById('save').addEventListener('click', saveOptions);
